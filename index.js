bdd1a20b0cdc500585ae58b901ed4ce753fe5ccc
Tasos Protopapas <tasos@ffyeah.gr>
2021-02-08 13:29:08 +0000

    cleanup

--- index.js
+++ index.js
@@ -14,74 +14,11 @@ let matches = [
 ];
 
 client.on('message', msg => {
-
-Object.values(matches).forEach( function(value){
-if (msg.content.endsWith(value)) {
-    msg.reply(aris_response);
-  }
-}
-)
+  Object.values(matches).forEach( function(value){
+    if (msg.content.endsWith(value)) {msg.reply(aris_response);}
+      })
 });
-/*
-  if (msg.content.endsWith('oni')) {
-    msg.reply('Μπάτσοι, γουρούνια δολοφόνοι!');
-  }
-  if (msg.content.endsWith('onei')) {
-    msg.reply('Μπάτσοι, γουρούνια δολοφόνοι!');
-  }
-  if (msg.content.endsWith('ώνει')) {
-    msg.reply('Μπάτσοι, γουρούνια δολοφόνοι!');
-  }
-  if (msg.content.endsWith('όνοι')) {
-    msg.reply('Μπάτσοι, γουρούνια δολοφόνοι!');
-  }
-  if (msg.content.endsWith('onoi')) {
-    msg.reply('Μπάτσοι, γουρούνια δολοφόνοι!');
-  }  
-  if (msg.content.endsWith('ονοι')) {
-    msg.reply('Μπάτσοι, γουρούνια δολοφόνοι!');
-  }
-  if (msg.content.endsWith('sony')) {
-    msg.reply('Μπάτσοι, γουρούνια δολοφόνοι!');
-  }    
-  if (msg.content.endsWith('ονει')) {
-    msg.reply('Μπάτσοι, γουρούνια δολοφόνοι!');
-  }
-  if (msg.content.endsWith('ονυ')) {
-    msg.reply('Μπάτσοι, γουρούνια δολοφόνοι!');
-  }
-   if (msg.content.endsWith('όνυ')) {
-    msg.reply('Μπάτσοι, γουρούνια δολοφόνοι!');
-  }
-  if (msg.content.endsWith('ονει')) {
-    msg.reply('Μπάτσοι, γουρούνια δολοφόνοι!');
-  }
-  if (msg.content.endsWith('όνι')) {
-    msg.reply('Μπάτσοι, γουρούνια δολοφόνοι!');
-  }
-  if (msg.content.endsWith('ονι')) {
-    msg.reply('Μπάτσοι, γουρούνια δολοφόνοι!');
-  }
-  if (msg.content.endsWith('ωνει')) {
-    msg.reply('Μπάτσοι, γουρούνια δολοφόνοι!');
-  }
-  if (msg.content.endsWith('ένωση')) {
-    msg.reply('Ένωση Κεντρώων!');
-  }
-  if (msg.content.endsWith('ένωση!')) {
-    msg.reply('Ένωση Κεντρώων!');
-  }
-  if (msg.content.endsWith('enosi')) {
-    msg.reply('Ένωση Κεντρώων!');
-  }
-  if (msg.content.endsWith('enosi!')) {
-    msg.reply('Ένωση Κεντρώων!');
-  }
-  */
-//});
 
-// Here you can login the bot. It automatically attempts to login the bot
-// with the environment variable you set for your bot token ("DISCORD_TOKEN")
 client.login();
 
 const http = require('http');
